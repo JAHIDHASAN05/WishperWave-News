991b8d19a581b97f3d8d8760b4374b1c1f64a108
JAHID HASAN <developerjahid@gmail.com>
2024-01-26 16:15:12 +0600

    left Nav category added

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+    const [category, setCategory]= useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:1212/category')
+        .then(res=> res.json())
+        .then(data=> setCategory(data))
+        .catch(error=>console.error(error))
+    },[])
+
+    // console.log(category)
     return (
-        <div>
-        <h1>LetNav</h1>
+        <div >
+            {
+                category.map(n=> <p key={n.id}>
+                    <Link to={`/category/${n.id}`}>{n.Category}</Link>
+                </p>)
+            }
         </div>
     );
 };
--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
+
+import { Button, ListGroup } from 'react-bootstrap';
 import React from 'react';
+import { FaFacebook, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
 const RightNav = () => {
     return (
         <div>
-            <h1>right nav</h1>
+            <div>
+                <h4>Login with</h4>
+
+                <Button className='mt-3' variant="outline-primary mb-2"> <FaGoogle /> Login With Google</Button>
+
+                <Button variant="outline-secondary"> <FaGithub /> Log in with GitHub</Button>
+            </div>
+            <div className='mt-4'>
+                <h3>Find on us</h3>
+                <ListGroup >
+                    <ListGroup.Item> <FaFacebook style={{color:"Blue"}} /> FaceBook</ListGroup.Item>
+                    <ListGroup.Item> <FaTwitter style={{color:'red'}}></FaTwitter> Twiter</ListGroup.Item>
+                    <ListGroup.Item> <FaInstagram></FaInstagram> Instragam</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
